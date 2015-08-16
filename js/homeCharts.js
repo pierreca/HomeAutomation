0b9c612ce40930b402539eb3aa34feb6c5ce8292
Pierre Cauchois <pierreca@Pierres-iMac.local>
2015-08-15 20:31:19 -0700

    Still ugly, but at least it works

--- js/homeCharts.js
+++ js/homeCharts.js
@@ -20,7 +20,7 @@ var xi_duration_intervalCount = {
   '5days' : 20
 };
 
-function drawCharts() {
+function drawAllCharts() {
   var durationSelect = document.getElementById('durations');
   var duration = durationSelect.options[durationSelect.selectedIndex].value;
   
@@ -32,9 +32,9 @@ function drawCharts() {
 function buildXivelyUrls(dataStreamNames, duration) {
   var datastreamParameter = "";
   var urls = [];
-  for (index = 0; index < dataStreamNames.length; index++) {
-    datastreamParameter += dataStreamNames[index];
-    if (index < dataStreamNames.length - 1) {
+  for (var i = 0; i < dataStreamNames.length; i++) {
+    datastreamParameter += dataStreamNames[i];
+    if (i < dataStreamNames.length - 1) {
       datastreamParameter += ",";
     }
   }
@@ -42,80 +42,88 @@ function buildXivelyUrls(dataStreamNames, duration) {
   var now = moment();
   var endDate = now.toISOString();
   
-  for (i = 1; i <= xi_duration_intervalCount[duration]; i++) {
+  for (var i = 1; i <= xi_duration_intervalCount[duration]; i++) {
     var startDate = now.subtract(6, 'hours').toISOString();
   
-    var xi_url = 'https://api.xively.com/v2/feeds/' + xi_feed_id + '.json?key=' + xi_api_key + '&datastreams=' + datastreamParameter + '&start=' + startDate + '&end=' + endDate + '&limit=1000&find_previous'
+    var xi_url = 'https://api.xively.com/v2/feeds/' + xi_feed_id + '.json?key=' + xi_api_key + '&datastreams=' + datastreamParameter + '&start=' + startDate + '&end=' + endDate + '&limit=1000'
     urls.push(xi_url);
     console.log("URL: " + xi_url);
     endDate = startDate;
   }
   
-  return urls;  
+  return urls.reverse();  
+}
+
+function createChart(sensors, divId, measureName, dataSubsets) {
+  var dataTemp = new google.visualization.DataTable();
+        
+  dataTemp.addColumn('datetime', 'Time');
+  for (var i = 0; i < sensors.length; i++) {
+    dataTemp.addColumn('number', sensors[i]);
+  }
+  
+  for (var i = 0; i < dataSubsets.length; i++) {
+    for (var j = 0; j < dataSubsets[i].length; j++) {
+      dataTemp.addRow(dataSubsets[i][j]);
+    }
+  }
+  
+  var chart = new google.charts.Line($(divId).get(0));
+  var options = {
+      title: measureName,
+      width: 1200,
+      height: 400,
+      series: {
+          0: {targetAxisIndex: 0}
+      }
+    };
+
+  chart.draw(dataTemp, google.charts.Line.convertOptions(options));
 }
 
 function drawAggregatedSensorsChart(sensors, measureName, divId, duration) {
   var datastreams = [];
-  for (i = 0; i < sensors.length; i++) {
+  for (var i = 0; i < sensors.length; i++) {
     datastreams.push(sensors[i] + '_' + measureName);
   }
   console.log(datastreams);
   
   var xi_urls = buildXivelyUrls(datastreams, duration);
-  
   var dataSubsets = [];
   
-  function downloadPage(pageIndex) {
-    var jsonData = $.ajax({
-      url: xi_urls[pageIndex]
-    }).done(function (results) {
+  function getXivelyDataPage (index) {
+    var def = $.Deferred();
+    $.getJSON(xi_urls[index], function (results) {
       var subset = [];
       $.each(results.datastreams[0].datapoints, function (i, row) {
         try {
           var cols = [(new Date(row.at))];
-          for (j = 0; j < sensors.length; j++) {
+          for (var j = 0; j < sensors.length; j++) {
             cols.push(parseFloat(results.datastreams[j].datapoints[i].value));
           }
+          
           subset.push(cols);
-        }
-        catch (err) {
+        } catch (err) {
           console.log(err);
+          def.reject(err);
         }
       });
-      dataSubsets.push(subset);
-      pageIndex++;
-      if (pageIndex < xi_urls.length) {
-        downloadPage(pageIndex);
-      } else {
-        var dataTemp = new google.visualization.DataTable();
-        
-        dataTemp.addColumn('datetime', 'Time');
-        for (i = 0; i < sensors.length; i++) {
-          dataTemp.addColumn('number', sensors[i]);
-        }
-        
-        for (i = 0; i < dataSubsets.length; i++) {
-          for (j = 0; j < dataSubsets[i].length; j++) {
-            dataTemp.addRow(dataSubsets[i][j]);
-          }
-        }
-        
-        var chart = new google.charts.Line($(divId).get(0));
-        var options = {
-            title: measureName,
-            width: 1200,
-            height: 400,
-            series: {
-                0: {targetAxisIndex: 0}
-            }
-          };
-
-        chart.draw(dataTemp, google.charts.Line.convertOptions(options));
-      }
+      
+      dataSubsets[index] = subset;
+      def.resolve(index);
     });
+    
+    return def;
   }
   
-  downloadPage(0);
+  var xi_requests = [];
+  for (var i = 0; i < xi_urls.length; i++) {
+    xi_requests.push(getXivelyDataPage(i));
+  }
+  
+  $.when.apply($, xi_requests).then(function() {
+    createChart(sensors, divId, measureName, dataSubsets);
+  });
 }
 
 // load chart lib
@@ -123,5 +131,5 @@ google.load('visualization', '1.1', {
   packages: ['line']
 });
 
-// call drawChart once google charts is loaded
-google.setOnLoadCallback(drawCharts);
+// call drawAllCharts once google charts is loaded
+google.setOnLoadCallback(drawAllCharts);
